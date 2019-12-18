0833810ba0a054e34682d267d620b7ab50c0d80f
Luis PM <luisp237@gmail.com>
2019-12-17 19:56:17 -0500

    completed closure function

--- index.js
+++ index.js
@@ -257,10 +257,11 @@ function tallyUpDonations(runners) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
-  }
+  let count = -1;
+  return () => ++count;
+    
+    
+  
   // BROKEN CODE ENDS
 }
 
